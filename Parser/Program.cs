2b32fda1b9b1dc2c69425290b5f00ff39f056798
Thomas Williams <thomas_zarrow@outlook.com>
2018-08-17 14:53:41 +0100

    Just trying out a separate cutscene parser

---
+++ Parser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Joey => WP1");
+            sb.AppendLine("Lucy => WP3");
+
+            var parser = new GalvosCutsceneParser(sb.ToString());
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
