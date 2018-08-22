889b7ed5b1bc2b8c300d071b1697d63082d354fa
Thomas Williams <thomas_zarrow@outlook.com>
2018-08-22 20:49:25 +0100

    WIP - building out the turn in direction step

---
+++ GalvosCutsceneParser/Chunks/TurnVectorStep.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using UnityEngine;
+
+namespace GalvosCutsceneParser
+{
+    public class TurnVectorStep : BaseStep
+    {
+        public enum Direction
+        {
+            North = 0, South = 1, East = 2, West = 3
+        }
+
+        private CutsceneEntity entity;
+        private Direction dir;
+
+        public TurnVectorStep(CutsceneEntity entity, Direction dir)
+        {
+            this.dir = dir;
+            this.entity = entity;
+        }
+
+        protected override List<XAttribute> GetRootNodeAttributes(int index, bool isFinal)
+        {
+            return new List<XAttribute>()
+            {
+                new XAttribute("direction", (int)this.dir)
+            }.Concat(base.GetRootNodeAttributes(index, isFinal)).ToList();
+        }
+
+        protected override List<XAttribute> GetBooleanAttributes()
+        {
+            return new List<XAttribute>()
+            {
+                new XAttribute("active", true.ToString()),
+                new XAttribute("overrideNodeName", false.ToString())
+            };
+        }
+
+        protected override List<XElement> GetStringElements()
+        {
+            var nodeType = new XElement("_type");
+            nodeType.Add(new XCData("GalvosTurnVector"));
+
+            return base.GetStringElements().Concat(new List<XElement>()
+            {
+                nodeType
+            }).ToList();
+        }
+
+        protected override List<XElement> GetStringArrayElements()
+        {
+            return new List<XElement>();
+        }
+
+        protected override List<XElement> GetNodeSpecialElements()
+        {
+            var emptyChildName = new XElement("childName");
+            emptyChildName.Add(new XCData(""));
+
+            var emptyValue = new XElement("value");
+            emptyValue.Add(new XCData(""));
+
+            return new List<XElement>()
+            {
+                new XElement("usedObject",
+                        new XAttribute("type", 0),
+                        new XAttribute("aID", this.entity.ID),
+                        new XAttribute("wID", 0),
+                        new XAttribute("pID", 0),
+                        new XAttribute("pID2", -1),
+                    new XElement("_string", emptyChildName),
+                    new XElement("objectKey",
+                            new XAttribute("type", 0),
+                        new XElement("_string", emptyValue))),
+            };
+        }
+    }
+}
---
+++ ParserTests/Chunks/TurnVectorStepTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using GalvosCutsceneParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParserTests
+{
+    [TestClass]
+    public class TurnVectorStepTest
+    {
+        [TestMethod]
+        public void TestCorrectXMLGenerated()
+        {
+            string expected = "<8 direction=\"0\" next=\"-1\">" +
+                "<_bool active=\"True\" overrideNodeName=\"False\" />" +
+                "<_string>" +
+                    "<nodeName><![CDATA[]]></nodeName>" +
+                    "<_type><![CDATA[GalvosTurnVector]]></_type>" +
+                "</_string>" +
+                "<usedObject type=\"0\" aID=\"0\" wID=\"0\" pID=\"0\" pID2=\"-1\">" +
+                    "<_string>" +
+                        "<childName><![CDATA[]]></childName>" +
+                    "</_string>" +
+                    "<objectKey type=\"0\">" +
+                        "<_string>" +
+                            "<value><![CDATA[]]></value>" +
+                        "</_string>" +
+                    "</objectKey>" +
+                "</usedObject>" +
+            "</8>";
+
+            var step = new TurnVectorStep(new CutsceneEntity(0), TurnVectorStep.Direction.North);
+            Assert.AreEqual(expected, step.ToXML(8, true).ToString().ConvertValidXMLToORK());
+        }
+    }
+}
