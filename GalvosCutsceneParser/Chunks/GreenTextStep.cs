49f74305ed5879e4801dfd3341075f1b459d296e
Thomas Williams <thomas_zarrow@outlook.com>
2019-04-07 18:43:49 +0100

    Created green text step

---
+++ GalvosCutsceneParser/Chunks/GreenTextStep.cs
@@ -0,0 +1,26 @@
+﻿using GalvosCutsceneParser.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GalvosCutsceneParser
+{
+    public class GreenTextStep : BaseStep
+    {
+        public string Message { get; private set; }
+
+        public static bool IsMatch(List<string> chunks)
+        {
+            return chunks[0] == ">>";
+
+        }
+
+        public GreenTextStep(List<string> chunks, IEntitySupplier entitySupplier)
+        {
+            string s = String.Join(" ", chunks.ToArray());
+            string withoutquotes = RegexUtilities.PullOutTextInsideQuotes(ref s);
+            this.Message = withoutquotes;
+        }
+    }
+}
